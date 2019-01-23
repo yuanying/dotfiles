fd564e1cbf8b8e775fa28434959e309836e79596
OTSUKA, Yuanying <yuanying@fraction.jp>
2019-01-23 14:13:58 +0900

    Fix setup.sh

--- bin/setup.sh
+++ bin/setup.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
-ln -s $(pwd)/../bash_aliases ~/.bash_aliases
-ln -s $(pwd)/../zshrc ~/.zshrc
+ROOT=$(dirname "${BASH_SOURCE}")
+
+ln -s ${ROOT}/bash_aliases ~/.bash_aliases
+ln -s ${ROOT}/../zshrc ~/.zshrc
