3980024526f9eafaa2ec67650ec5188be9e2945d
OTSUKA, Motohiro/Yuanying <yuanying@fraction.jp>
2019-01-24 06:00:03 +0000

    Add gitconfig and tmux.conf

--- bin/setup.sh
+++ bin/setup.sh
@@ -2,6 +2,8 @@
 
 ROOT=$(dirname "${BASH_SOURCE}")
 
+ln -s ${ROOT}/../gitconfig ~/.gitconfig
+ln -s ${ROOT}/../tmux.conf ~/.tmux.conf
 ln -s ${ROOT}/../bash_aliases ~/.bash_aliases
 ln -s ${ROOT}/../zshrc ~/.zshrc
 ln -s ${ROOT}/../zshenv ~/.zshenv
