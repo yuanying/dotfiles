0885950368a515a551f1dcc9990e6f095291611c
OTSUKA, Yuanying <mototsuk@zlab.co.jp>
2019-02-08 14:03:14 +0900

    Add kube-ps1 to PROMPT

--- zsh.d/45_prompt.zsh
+++ zsh.d/45_prompt.zsh
@@ -4,6 +4,7 @@ COLOR_FG="%F{081}"
 COLOR_FG_FAIL="%F{197}"
 
 local ret_status="%(?:${COLOR_FG}# :${COLOR_FG_FAIL}# )"
-PROMPT="${ret_status} %F{228}%c%f \$(vcs_info_msg) %f"
+PROMPT='%F{228}%~%f $(vcs_info_msg)$(kube_ps1)
+${ret_status}%f'
 #RPROMPT='%{$fg[blue]%}($ZSH_KUBECTL_PROMPT)%{$reset_color%}'
 setopt transient_rprompt
