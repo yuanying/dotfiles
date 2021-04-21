5d1120a577384bc6b9be3602eb4d07bc62616fe5
OTSUKA, Yuanying <mototsuk@zlab.co.jp>
2021-04-21 06:30:14 +0000

    Support history-search-multi-word

--- zsh.d/extentions/peco_select_history.zsh
+++ zsh.d/extentions/peco_select_history.zsh
@@ -1,7 +1,11 @@
-function select-history() {
-  BUFFER=$(history -n -r 1 | fzf --no-sort +m --query "$LBUFFER" --prompt="History > ")
-  CURSOR=$#BUFFER
-}
+if [[ -f "${HOME}/.zsh/history-search-multi-word/history-search-multi-word.plugin.zsh" ]]; then
+    source "${HOME}/.zsh/history-search-multi-word/history-search-multi-word.plugin.zsh"
+else
+    function select-history() {
+    BUFFER=$(history -n -r 1 | fzf --no-sort +m --query "$LBUFFER" --prompt="History > ")
+    CURSOR=$#BUFFER
+    }
 
-zle -N select-history
-bindkey '^R' select-history
+    zle -N select-history
+    bindkey '^R' select-history
+fi
