0793b9619d31512fc0bda84adcbcb2479d8b7f60
OTSUKA, Yuanying <mototsuk@zlab.co.jp>
2021-08-16 12:47:59 +0000

    Fix kubectl completion

--- zsh.d/extentions/kubectl.zsh
+++ zsh.d/extentions/kubectl.zsh
@@ -2,8 +2,11 @@ if which kubectl >/dev/null 2>&1; then
   alias k='kubectl'
 
   ## Completion
-  source <(kubectl completion zsh)
-  complete -o default -F __start_kubectl k
+  if [[ ! -e "${fpath[1]}/_kubectl" ]]; then
+    kubectl completion zsh > "${fpath[1]}/_kubectl"
+  fi
+  # source <(kubectl completion zsh)
+  # complete -o default -F __start_kubectl k
 
 fi
 
