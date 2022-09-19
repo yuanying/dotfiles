6caa8d1ae2ed935cdc0cb27c34430b5bc61f905e
OTSUKA, Yuanying <mototsuk@zlab.co.jp>
2022-09-19 07:51:59 +0000

    Delete unused sections

--- spaceshiprc.zsh
+++ spaceshiprc.zsh
@@ -1,3 +1,26 @@
+SPACESHIP_PROMPT_ORDER=(
+  time          # Time stampts section
+  user          # Username section
+  dir           # Current directory section
+  git           # Git section (git_branch + git_status)
+  node          # Node.js section
+  ruby          # Ruby section
+  python        # Python section
+  golang        # Go section
+  rust          # Rust section
+  haskell       # Haskell Stack section
+  julia         # Julia section
+  docker        # Docker section
+  venv          # virtualenv section
+  kubectl       # Kubectl context section
+  terraform     # Terraform workspace section
+  exec_time     # Execution time
+  line_sep      # Line break
+  jobs          # Background jobs indicator
+  exit_code     # Exit code section
+  char          # Prompt character
+)
+
 SPACESHIP_HOST_SHOW=false
 SPACESHIP_USER_SHOW=needed
 SPACESHIP_GOLANG_SHOW=false
