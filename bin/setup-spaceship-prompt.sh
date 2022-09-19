c46790b59e9d2a84db4d03084b098e445e90cd9c
OTSUKA, Yuanying <mototsuk@zlab.co.jp>
2022-09-19 07:11:40 +0000

    Delete unnecessary spaceship setup script

--- bin/setup-spaceship-prompt.sh
+++ bin/setup-spaceship-prompt.sh
@@ -2,4 +2,3 @@
 
 git clone https://github.com/denysdovhan/spaceship-prompt.git ~/.zsh/spaceship-prompt
 mkdir -p ~/.zsh/zsh-completions
-ln -sf ~/.zsh/spaceship-prompt/spaceship.zsh ~/.zsh/zsh-completions/prompt_spaceship_setup
