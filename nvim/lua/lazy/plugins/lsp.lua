f4e6607fc0203f26a7efbe2948e189afe9b92e2b
OTSUKA, Yuanying <mototsuk@zlab.co.jp>
2024-08-22 13:54:58 +0900

    Copilot tab

--- nvim/lua/lazy/plugins/lsp.lua
+++ nvim/lua/lazy/plugins/lsp.lua
@@ -110,7 +110,7 @@ return {
           -- ['<C-Space>'] = cmp.mapping.complete(),
           ['<C-e>'] = cmp.mapping.abort(),
           ['<CR>'] = cmp.mapping.confirm({ select = false }),
-          ['<Tab>'] = cmp.mapping.confirm({ select = true }),
+          -- ['<Tab>'] = cmp.mapping.confirm({ select = true }),
         }),
         sources = cmp.config.sources({
           { name = 'nvim_lsp' },
