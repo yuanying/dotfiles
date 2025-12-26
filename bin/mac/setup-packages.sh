64a4002d3feefba8dadab8bf9ad156be60f97a2d
OTSUKA, Yuanying <yuanying@fraction.jp>
2025-12-26 15:04:18 +0900

    Fix setup script for mac

--- bin/mac/setup-packages.sh
+++ bin/mac/setup-packages.sh
@@ -1,7 +1,23 @@
 #!/bin/bash
 
+ROOT=$(dirname "${BASH_SOURCE}")
+cd ${ROOT}/..
+ROOT=$(pwd)
+
 brew install ripgrep
 brew install fd
+brew install terminal-notifier
+brew install mosh
+brew install tree-sitter
+brew install gojq
 
 brew tap homebrew/cask-fonts
 brew install --cask font-hack-nerd-font
+
+rm -f ~/bin/notify_server.py
+ln -s ${ROOT}/bin/notify_server.py ~/bin/notify_server.py
+
+if [[ ! -f ~/Library/LaunchAgents/com.local.notify.plist ]]; then
+    ln -s ${ROOT}/LaunchAgents/com.local.notify.plist \
+      ~/Library/LaunchAgents/com.local.notify.plist
+fi
